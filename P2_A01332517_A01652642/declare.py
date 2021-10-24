f8db22b22e1d550fce9f95cbca03a809bbe2aecf
Emiliano Pineda <emi.rock11@hotmail.com>
2021-10-23 20:38:52 -0500

    Tarea casi completada :(

--- P2_A01332517_A01652642/declare.py
+++
@@ -1,9 +0,0 @@
-from antlr.CoolListener import CoolListener
-from antlr.CoolParser import CoolParser
-
-
-class Declarations(CoolListener):
-    idTable = {}
-
-    def exitAtribute(self, ctx: CoolParser.AtributeContext):
-        self.idtable[ctx.ID().getText()] = ctx.TYPE().getText()
